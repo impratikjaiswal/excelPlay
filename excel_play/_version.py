5c1bf00086d8f90f77b2adfff7c2397dc9542ab9
imPratik <impratikjaiswal@gmail.com>
2024-10-20 22:35:18 +0530

    v2.4.4

--- excel_play/_version.py
+++ excel_play/_version.py
@@ -7,5 +7,5 @@ Provides excel_play version information.
 
 from incremental import Version
 
-__version__ = Version("excel_play", 2, 4, 3)
+__version__ = Version("excel_play", 2, 4, 4)
 __all__ = ["__version__"]
